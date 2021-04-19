883e3fe574f439aec1b2d09961fab6a00a6cf1c3
limeng01 <limeng01@jiuqi.com.cn>
2021-04-19 16:49:36 +0800

    实现一个插入式注解器

--- spring-boot-demo/src/main/java/com/example/jvm/NameCheckProcessor.java
+++ spring-boot-demo/src/main/java/com/example/jvm/NameCheckProcessor.java
@@ -1,27 +1,44 @@
 package com.example.jvm;
 
-import javax.annotation.processing.*;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
 
-@SupportedAnnotationTypes("*")
-@SupportedSourceVersion(SourceVersion.RELEASE_8)
+/**
+ * 代码命名规范校验注解处理器
+ */
+@SupportedAnnotationTypes("*")// 表示对哪些注解感兴趣 
+@SupportedSourceVersion(SourceVersion.RELEASE_8)// 需要处理哪个版本的Java代码 
 public class NameCheckProcessor extends AbstractProcessor {
-    private  NameChecker nameChecker;
+
+    private NameChecker nameChecker;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
+        nameChecker = new NameChecker(processingEnv);
     }
 
-    @Override
+    /**
+     * 对输入的语法树的各个节点进行名称检查
+     *
+     * @param annotations 获取此注解处理器要处理的注解集合
+     * @param roundEnv    从该参数访问到当前这个轮次（Round）中的抽象语法树节点
+     * @return
+     */
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         if (!roundEnv.processingOver()) {
-            for (TypeElement element : annotations) {
+            for (Element element : roundEnv.getRootElements()) {
                 nameChecker.checkNames(element);
             }
         }
+        // 返回false通知该轮次中代码并未改变 
         return false;
     }
-}
+} 
--- spring-boot-demo/src/main/java/com/example/jvm/NameChecker.java
+++ spring-boot-demo/src/main/java/com/example/jvm/NameChecker.java
@@ -3,60 +3,134 @@ package com.example.jvm;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.ElementScanner8;
-import javax.tools.Diagnostic;
+
+import java.util.EnumSet;
+
+import static javax.tools.Diagnostic.Kind.WARNING;
 
 public class NameChecker {
     private final Messager messager;
-    NameCheckScanner nameCheckScanner = new NameCheckScanner();
 
-    public NameChecker(ProcessingEnvironment processingEnvironment) {
-        this.messager = processingEnvironment.getMessager();
+    private NameCheckScanner nameCheckScanner = new NameCheckScanner();
+
+    public NameChecker(ProcessingEnvironment processingEnv) {
+        this.messager = processingEnv.getMessager();
     }
 
-    public void checkNames(Element e) {
-        nameCheckScanner.scan(e);
+    /**
+     * @param element
+     */
+    public void checkNames(Element element) {
+        nameCheckScanner.scan(element);
     }
 
     private class NameCheckScanner extends ElementScanner8<Void, Void> {
 
+        /**
+         * 检查类名是否合法
+         *
+         * @param e
+         * @param p
+         * @return
+         */
         @Override
-        public Void visitType(TypeElement e, Void unused) {
-            scan(e.getTypeParameters(), unused);
-            return super.visitType(e, unused);
+        public Void visitType(TypeElement e, Void p) {
+            scan(e.getTypeParameters(), p);
+            checkCamelCase(e, true);
+            super.visitType(e, p);
+            return null;
         }
 
+        /**
+         * 检查方法命名是否合法
+         *
+         * @param e
+         * @param p
+         * @return
+         */
         @Override
-        public Void visitExecutable(ExecutableElement e, Void unused) {
-            return super.visitExecutable(e, unused);
+        public Void visitExecutable(ExecutableElement e, Void p) {
+            if (e.getKind() == ElementKind.METHOD) {
+                Name name = e.getSimpleName();
+                if (name.contentEquals(e.getEnclosingElement().getSimpleName())) {
+                    messager.printMessage(WARNING, "一个普通方法 " + name + " 不应当与类名重复，避免与构造函数产生混淆", e);
+                }
+                checkCamelCase(e, false);
+            }
+            super.visitExecutable(e, p);
+            return null;
         }
 
+        /**
+         * 检查变量命名是否合法
+         *
+         * @param e
+         * @param p
+         * @return
+         */
         @Override
-        public Void visitVariable(VariableElement e, Void unused) {
-            return super.visitVariable(e, unused);
+        public Void visitVariable(VariableElement e, Void p) {
+            // 如果这个变量是常量或枚举，则按照大写命名检查，否则按照驼式命名法规则检查 
+            if (e.getKind() == ElementKind.ENUM_CONSTANT || e.getConstantValue() != null ||
+                    heuristicallyConstant(e)) {
+                checkAllCaps(e);
+            } else {
+                checkCamelCase(e, false);
+            }
+            return null;
         }
 
+        /**
+         * 判断一个变量是否为常量
+         *
+         * @param e
+         * @return
+         */
+        private boolean heuristicallyConstant(VariableElement e) {
+            if (e.getEnclosingElement().getKind() == ElementKind.INTERFACE) {
+                return true;
+            } else if (e.getKind() == ElementKind.FIELD &&
+                    e.getModifiers().containsAll(EnumSet.of(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL))) {
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * 检查传入的Element是否符合驼峰命名法，如果不符合输出警告信息
+         *
+         * @param e
+         * @param initialCaps
+         */
         private void checkCamelCase(Element e, boolean initialCaps) {
             String name = e.getSimpleName().toString();
+            // 上个字母是否大写
             boolean previousUpper = false;
             boolean conventional = true;
             int firstCodePoint = name.codePointAt(0);
+
             if (Character.isUpperCase(firstCodePoint)) {
                 previousUpper = true;
                 if (!initialCaps) {
-                    messager.printMessage(Diagnostic.Kind.WARNING, "名称" + name + "应该以小写字母开头", e);
+                    messager.printMessage(WARNING, "名称 " + name + " 应该以小写字母开头", e);
                     return;
                 }
             } else if (Character.isLowerCase(firstCodePoint)) {
                 if (initialCaps) {
-                    messager.printMessage(Diagnostic.Kind.WARNING, "名称" + name + "应该以大写字母开头", e);
+                    messager.printMessage(WARNING, "名称 " + name + " 应该以大写字母开头", e);
                     return;
                 }
-            } else
+            } else {
                 conventional = false;
+            }
+
             if (conventional) {
                 int cp = firstCodePoint;
                 for (int i = Character.charCount(cp); i < name.length(); i += Character.charCount(cp)) {
@@ -67,27 +141,38 @@ public class NameChecker {
                             break;
                         }
                         previousUpper = true;
+                    } else {
+                        previousUpper = false;
                     }
-                    previousUpper = false;
                 }
             }
-            if (!conventional)
-                messager.printMessage(Diagnostic.Kind.WARNING, "名称" + name + "应符合驼峰式命名法", e);
+
+            if (!conventional) {
+                messager.printMessage(WARNING, "名称 " + name + " 应该符合驼式命名法(Camel Case Names)", e);
+            }
         }
 
+        /**
+         * 大写命名检查
+         * 要求第一个字母必须是大写的英文字母，其余部门可以是下划线或大写字母
+         *
+         * @param e
+         */
         private void checkAllCaps(Element e) {
             String name = e.getSimpleName().toString();
             boolean conventional = true;
             int firstCodePoint = name.codePointAt(0);
-            if (!Character.isUpperCase(firstCodePoint))
+            if (!Character.isUpperCase(firstCodePoint)) {
+                // 第一个字符不是大写字母
                 conventional = false;
-            else {
+            } else {
                 boolean previousUnderscore = false;
                 int cp = firstCodePoint;
                 for (int i = Character.charCount(cp); i < name.length(); i += Character.charCount(cp)) {
                     cp = name.codePointAt(i);
                     if (cp == (int) '_') {
                         if (previousUnderscore) {
+                            // 连续两个_
                             conventional = false;
                             break;
                         }
@@ -101,8 +186,10 @@ public class NameChecker {
                     }
                 }
             }
-            if (!conventional)
-                messager.printMessage(Diagnostic.Kind.WARNING, "名称" + name + "应符合驼峰式命名法", e);
+
+            if (!conventional) {
+                messager.printMessage(WARNING, "常量 " + name + " 应该全部以大写字母或下划线命名，并且以字母开头", e);
+            }
         }
     }
 }
